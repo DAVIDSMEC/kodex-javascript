f4805b33298248c814cdf79cc65f3ebccf469e63
Eze David <ezedavid@Ezes-MacBook-Air.local>
2023-06-20 12:05:31 +0100

    Introduction to JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Values and Variables
+console.log("Hello World!");
+
+let firstName = "David";
+
+console.log(firstName);
+
+// Variable naming conventions
+let last_name2 = "John"
+let $function = 50;
+
+console.log($function);
+console.log(last_name2);
