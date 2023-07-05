7c4019fbfc45db27f25523f88010c2c566064f07
Eze David <ezedavid@Ezes-MacBook-Air.local>
2023-07-05 11:14:54 +0100

    ternary operator

--- script.js
+++ script.js
@@ -532,6 +532,20 @@ if(day === 'tuesday'){
 }
  
 
+// -------------------------The Ternary Operator------------------
+ const age = 23;
+ age >= 18 ? console.log('I love alchohol') : console.log('I like caprisun');
+
+//  OR
+// if (age >= 18) {
+// console.log('I love alchohol')
+// }
+// else {
+//   console.log('I like caprisun')
+// }
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`the bills ${bill}, the tips was ${tip}, and the total value is ${bill + tip}`)
 
 
 
